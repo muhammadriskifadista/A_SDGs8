47d4ffa895db9b91cf082f116b69c0738e9b67e2
muhammadriskifadista <55056755+muhammadriskifadista@users.noreply.github.com>
2020-12-21 20:00:12 +0700

    register

--- lib/Register.dart
+++ lib/Register.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sdgs8/NavBar.dart';
-import 'package:sdgs8/Register.dart';
+import 'package:sdgs8/Login.dart';
 
-class Login extends StatelessWidget {
+class Register extends StatelessWidget {
   Widget appBar() {
     return AppBar(
       title: Text('Share Lok.er'),
@@ -20,18 +19,6 @@ class Login extends StatelessWidget {
             children: <Widget>[
               Column(
                 children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(left: 20, right: 20, bottom: 30.0),
-                    height: 180,
-                    width: 180,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        image: DecorationImage(
-                          image: AssetImage('images/kerja.jpg'),
-                          alignment: Alignment(-0.3, 1.0),
-                        ),
-                        borderRadius: BorderRadius.circular(80.0)),
-                  ),
                   TextField(
                     decoration: InputDecoration(
                       hintText: "Masukan Nama Lengkap Anda",
@@ -40,7 +27,18 @@ class Login extends StatelessWidget {
                     ),
                   ),
                   Padding(
-                    padding: EdgeInsets.only(bottom: 20.0),
+                    padding: EdgeInsets.only(bottom: 5.0),
+                  ),
+                  //email
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Masukan email anda",
+                      icon: Icon(Icons.mail_outline),
+                      labelText: 'Email',
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 5.0),
                   ),
                   //Password TextField
                   TextField(
@@ -53,6 +51,17 @@ class Login extends StatelessWidget {
                   Padding(
                     padding: EdgeInsets.only(bottom: 20.0),
                   ),
+                  //no hp
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "No Handphone",
+                      icon: Icon(Icons.phone_android),
+                      labelText: 'No. Hp',
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 5.0),
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
@@ -61,19 +70,19 @@ class Login extends StatelessWidget {
                           padding: const EdgeInsets.only(
                             left: 20.0,
                             right: 20.0,
-                            bottom: 20.0,
+                            bottom: 10.0,
                           ),
                           child: RaisedButton(
                             onPressed: () {
                               Navigator.pushReplacement(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) => NavBar(),
+                                  builder: (context) => Login(),
                                 ),
                               );
                             },
                             child: Text(
-                              'Login',
+                              'Daftar',
                               style: TextStyle(
                                   color: Colors.white,
                                   fontWeight: FontWeight.bold),
@@ -87,45 +96,19 @@ class Login extends StatelessWidget {
                       ),
                     ],
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                            left: 20.0,
-                            right: 20.0,
-                            bottom: 20.0,
-                          ),
-                          child: RaisedButton(
-                            onPressed: () {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Register(),
-                                ),
-                              );
-                            },
-                            child: Text(
-                              'Register',
-                              style: TextStyle(
-                                  color: Colors.blue,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            color: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                            ),
-                          ),
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Login(),
                         ),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
+                      );
+                    },
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold),
                     ),
                   ),
                 ],
